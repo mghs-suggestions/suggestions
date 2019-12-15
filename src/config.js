92ee00a76454887d08aa171c9fde4fe7e867b110
yungyhan <yungyhan@gmail.com>
2019-12-15 23:37:25 +1100

    Included config (unsecure)

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAwbnphgY6PpnAkodYEf7hqui8_saWzXZU ",
+    discoveryDocs:
+        ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    clientId: "846305806889-17ohcnq23a6m2ls9bmtsbfhjore0fv8d.apps.googleusercontent.com",
+    scope: "https://www.googleapis.com/auth/spreadsheets",
+    spreadsheetId: "132DUSf25AAY0v8o-LcKGlfrKL_w4n3UKMkgkKeV4cjM"
+};
